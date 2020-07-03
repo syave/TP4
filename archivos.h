905f57102f65c04c9605c78e7e076a8c80e9a628
Samuel <67020037+syave@users.noreply.github.com>
2020-07-03 00:06:50 -0300

    Update archivos.h
    Agregue algunas funciones que faltan terminar

--- archivos.h
+++ archivos.h
@@ -9,5 +9,10 @@
 using namespace std;
 
 void cargarDesdeArchivo(std::string nombreArchivo, Lista<Pelicula> &lista);
+//Falta arreglar
+/*
+void cargarRecomendadas(Lista<Pelicula> &lista1, Lista<Pelicula> &lista2,Lista<Pelicula> &lista3);
+void escribirArchivo(ofstream* archivo, string nombre);
+*/
 
 #endif /* ARCHIVOS_H_ */
