9e737f5c69fd940f1cccde87387b71c485b41a5f
Samuel <67020037+syave@users.noreply.github.com>
2020-07-03 00:05:42 -0300

    Update archivos.cpp
    Agregue algunas funciones incompletas

--- archivos.cpp
+++ archivos.cpp
@@ -1,6 +1,34 @@
 #include "archivos.h"
 #include "Pelicula.h"
 
+//Faltan arreglos
+/*
+void escribirArchivo(ofstream* archivo, std::string nombre) {
+	*archivo << nombre << endl;
+}
+
+void cargarRecomendadas(Lista<Pelicula> &lista1, Lista<Pelicula> &lista2,Lista<Pelicula> &lista3) {
+	ofstream recomendado("recomendado.txt");
+	int puntos;
+
+	if (!recomendado.fail()) {
+		for (unsigned int j = 1; j <= lista1.getTamanio(); j++) {
+			for (unsigned int i = 1; i <= lista2.getTamanio(); i++) {
+				if((lista1.obtener(j).getGenero() == lista2.obtener(i).getGenero()) && (lista1.obtener(j).getDirector() == lista2.obtener(i).getDirector())) {
+					lista3.agregar(lista1.obtener(j));
+					escribirArchivo(&recomendado,lista1.obtener(j).getNombre());
+				}
+				puntos = atoi(lista2.obtener(i).getPuntaje().c_str());
+				if(puntos >= 7) {
+					lista3.agregar(lista2.obtener(i));
+					escribirArchivo(&recomendado,lista2.obtener(i).getNombre());
+				}
+
+			}
+		}
+	}
+}
+*/
 void cargarDesdeArchivo(std::string nombreArchivo, Lista<Pelicula> &lista) {
 	ifstream archivo(nombreArchivo);
 	string linea;
