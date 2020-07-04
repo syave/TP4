f4bd55833323d12489991c963a782d268f28c8ae
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2020-07-04 04:32:24 -0300

    Agrego metodos para el manejo de recomendaciones

--- Menu.h
+++ Menu.h
@@ -3,25 +3,44 @@
 
 #include "Lista.h"
 #include "Pelicula.h"
+#include <cstdlib>
 
 class Menu {
 private:
 	Lista<Pelicula> *peliculas_vistas;
 	Lista<Pelicula> *peliculas_no_vistas;
+	Lista<Pelicula> *peliculas_recomendadas;
 public:
-
 	Menu();
+	~Menu();
+
 	void mostrarMenu();
 
 	void agregarPeliculasVistas(Lista<Pelicula> *pelicula);
 	void agregarPeliculasNoVistas(Lista<Pelicula> *pelicula);
-	Lista<Pelicula> * obtenerPeliculasVistas();
-	Lista<Pelicula> * obtenerPeliculasNoVistas();
+	
+	Lista<Pelicula>* obtenerPeliculasVistas();
+	Lista<Pelicula>* obtenerPeliculasNoVistas();
+	Lista<Pelicula>* obtenerPeliculasRecomendadas();
 
 	void mostrarPeliculasVistas();
 	void mostrarPeliculasNoVistas();
+	void mostrarPeliculasRecomendadas();
+
+	void calcularRecomendadas();
+	
 private:
 	void mostrarElementos(Lista<Pelicula> * pelicula);
+
+	bool puntajeMayorA(Pelicula *pelicula, int puntaje);
+
+	bool coincidenGenero(Pelicula *peliculaA, Pelicula *peliculaB);
+
+	bool coincidenDirector(Pelicula *peliculaA, Pelicula *peliculaB);
+
+	bool coincidenActores(Pelicula *peliculaA, Pelicula *peliculaB);
+
+	bool coincideDirectorOActor(Pelicula *peliculaA, Pelicula *peliculaB);
 };
 
 
