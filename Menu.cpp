dbaae161744c816a5a1b98516c6608f5bc0b4575
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2020-07-04 04:33:31 -0300

    Agrego implementaciones de recomendaciones

--- Menu.cpp
+++ Menu.cpp
@@ -1,8 +1,15 @@
 #include "Menu.h"
 
 Menu::Menu(){
-	this->peliculas_vistas = 0;
-	this->peliculas_no_vistas = 0;
+	peliculas_vistas = 0;
+	peliculas_no_vistas = 0;
+	peliculas_recomendadas = 0;
+}
+
+Menu::~Menu() {
+		delete peliculas_vistas;
+		delete peliculas_no_vistas;
+		delete peliculas_recomendadas;
 }
 
 void Menu::mostrarMenu() {
@@ -29,7 +36,7 @@ void Menu::mostrarMenu() {
 			mostrarPeliculasNoVistas();
 			break;
 		case 3:
-			//TODO
+			mostrarPeliculasRecomendadas();
 			break;
 		}
 	}
@@ -59,6 +66,10 @@ void Menu::mostrarPeliculasNoVistas() {
 	mostrarElementos (peliculas_no_vistas);
 }
 
+void Menu::mostrarPeliculasRecomendadas() {
+	mostrarElementos (peliculas_recomendadas);
+}
+
 void Menu::mostrarElementos(Lista<Pelicula> * pelicula) {
 	for (unsigned int j = 1; j <= pelicula->getTamanio(); j++) {
 
@@ -75,4 +86,73 @@ void Menu::mostrarElementos(Lista<Pelicula> * pelicula) {
 		std::cout << std::endl;
 	}
 }
+void Menu::calcularRecomendadas(){
 
+	if (peliculas_vistas != 0 && peliculas_no_vistas != 0) {
+
+		peliculas_recomendadas = new Lista<Pelicula>;
+		Pelicula peliculaVista;
+		Pelicula peliculaNoVista;
+
+//		No se si es muy optimo pero es lo unico que se me ocurrio xd
+		for (unsigned int i = 1; i <= peliculas_vistas->getTamanio(); i++) {
+
+			peliculaVista = peliculas_vistas->obtener(i);
+
+			if (puntajeMayorA(&peliculaVista, 7)) {
+				peliculas_recomendadas->agregar(peliculaVista);
+			}
+
+			for (unsigned int j = 1; j <= peliculas_no_vistas->getTamanio(); j++) {
+
+				peliculaNoVista = peliculas_no_vistas->obtener(j);
+
+				if(coincidenDirector(&peliculaVista, &peliculaNoVista)){
+
+					peliculas_recomendadas->agregar(peliculaNoVista);
+				}
+/*
+* 		A SOLUCIONAR: deberia (creo) poder verificar que coincidan
+* 			          director o actores con esta funcion, pero
+* 			          los elementos dentro de la lista recomendadas
+* 			          se duplican..
+*
+				if(coincideDirectorOActor(&peliculaVista, &peliculaNoVista)){
+
+					peliculas_recomendadas->agregar(peliculaNoVista);
+				}
+*/
+			}
+		}
+	}
+}
+
+bool Menu::puntajeMayorA(Pelicula *pelicula, int puntaje){
+	return (atoi(pelicula->getPuntaje().c_str()) >= puntaje);
+}
+
+bool Menu::coincidenGenero(Pelicula *peliculaA, Pelicula *peliculaB) {
+	return (peliculaA->getGenero() == peliculaB->getGenero());
+}
+
+bool Menu::coincidenDirector(Pelicula *peliculaA, Pelicula *peliculaB) {
+	return (peliculaA->getDirector() == peliculaB->getDirector());
+}
+
+bool Menu::coincidenActores(Pelicula *peliculaA, Pelicula *peliculaB) {
+	bool hayCoincidendia = false;
+
+	for (unsigned int i = 0; i <= peliculaA->getActores()->getTamanio(); i++) {
+		for (unsigned int j = 0; j <= peliculaB->getActores()->getTamanio(); j++) {
+			if (peliculaA->getActores()->obtener(i) == peliculaB->getActores()->obtener(j)) {
+				hayCoincidendia = true;
+			}
+		}
+	}
+
+	return hayCoincidendia;
+}
+
+bool Menu::coincideDirectorOActor(Pelicula *peliculaA, Pelicula *peliculaB) {
+	return (coincidenDirector(peliculaA, peliculaB)) || coincidenActores(peliculaA, peliculaB);
+}
